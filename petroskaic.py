e7106970c1d068f2f5fc35bef12a306785912710
Petras Anskaitis <sartep1996@gmail.com>
2023-04-17 08:44:06 +0300

    04.17

--- petroskaic.py
+++ petroskaic.py
@@ -4,12 +4,10 @@ print("Jusu atlyginimas: " + str(atlyginimas))
 
 perdiena = float(input("Kiek išleidžiate per dieną: "))
 
-
 dienos = 0
 
 while atlyginimas > 0:
     atlyginimas = atlyginimas - perdiena
     dienos = dienos + 1
-
     
-print("Reikės", dienos, "kad išleistumėte visą atlyginimą" )
+print("Reikės", dienos, "kad išleistumėte visą atlyginimą")
